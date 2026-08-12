b9c0a5c7785489ffbb845e246e856f5bd882fb96
Rushil Patel <rushilp274@gmail.com>
2026-08-12 13:43:14 +0530

    Time: 17 ms (74.18%), Space: 82.8 MB (29.61%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,36 +1,39 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if(n<(long)m*k){
-            return -1;
+       if ((long) m * k > bloomDay.length) {
+            return -1;  
         }
+        
         int low = Integer.MAX_VALUE;
         int high = Integer.MIN_VALUE;
-        for (int day : bloomDay) {
-            low = Math.min(low, day);
-            high = Math.max(high, day);
+        for(int b:bloomDay){
+            low=Math.min(low,b);
+            high=Math.max(high,b);
         }
         while(low<high){
             int mid = low+(high-low)/2;
-            int bouquet=0;
-            int cosFlowers=0;
-            for (int day : bloomDay) {
-                if (day <= mid) {
-                    cosFlowers++;
-                    if (cosFlowers == k) {
-                        bouquet++;
-                        cosFlowers = 0;
-                    }
-                } else {
-                    cosFlowers = 0;
-                }
-            }
-            if(bouquet>=m){
-                high=mid;
+            if(help(bloomDay,m,k,mid)){
+                high =mid;
             }else{
                 low=mid+1;
             }
         }
         return low;
+    }
+    public boolean help(int[] bloomDay, int m, int k, int mid){
+        int cnt=0;
+        int bCount=0;
+        for(int day:bloomDay){
+            if(day<=mid){
+                cnt++;
+                if(cnt==k){
+                    bCount++;
+                    cnt=0;
+                }
+            }else{
+                    cnt=0;
+                }
+        }
+        return bCount>=m;
     }
 }
