a71df4f4876183d50caa536b5b9bf109d5d871e1
Rushil Patel <rushilp274@gmail.com>
2026-08-13 10:48:38 +0530

    Time: 0 ms (100%), Space: 42.9 MB (84.3%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,40 +1,34 @@
 class Solution {
-    public int splitArray(int[] arr, int k) {
-        long low = -1;
-        long high = 0;
-        long res =0;
-        if(arr.length<k){
-            return -1;
-        }
-        for(int i = 0; i < arr.length; i++){
-            low = Math.max(low,arr[i]);
-            high += arr[i];
+    public int splitArray(int[] nums, int k) {
+        long low =-1;
+        long high =0;
+        long res =-1;
+        for(int i:nums){
+            low=Math.max(low,i);
+            high+=i;
         }
         while(low<=high){
             long mid = low+(high-low)/2;
-            if(help(mid,arr,k)){
-                res=mid;
-                high = mid-1;
+            if(help(nums,k,mid)){
+                res =mid;
+                high =mid-1;
             }else{
-                low=mid+1;
+                low = mid+1;
             }
         }
         return (int)res;
     }
-    public boolean help(long mid,int[] arr,int k){
-        int allocatedStudent=1;
-        long pages =0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>mid){
-                return false;
-            }
-            if(pages+arr[i]>mid){
-                allocatedStudent++;
-                pages=arr[i];
+    public boolean help(int[] nums,int k, long mid){
+        int s=1;
+        long sum=0;
+        for(int ss:nums){
+            if(sum+ss>mid){
+                s++;
+                sum=ss;
             }else{
-                pages+=arr[i];
+                sum+=ss;
             }
         }
-        return (allocatedStudent<=k);
+        return s<=k;
     }
 }
